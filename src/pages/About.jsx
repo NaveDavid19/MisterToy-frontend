0edb14083b497ba0ed28138015ed7dda15d0b14e
NaveDavid19 <navedavid16@gmail.com>
2024-01-01 20:23:28 +0200

    Add about info

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,19 @@
 export function About() {
   return (
-    <section>
+    <section className="about-container">
       <h2>About Us</h2>
+      <img src="..\src\assets\img\nave.jpg" alt="" />
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni aperiam
-        quo veniam velit dolor reprehenderit, laudantium consequatur neque
-        numquam labore quae. Accusamus libero perferendis ducimus? Alias unde
-        hic quisquam doloremque.
+        My name is Nave David. im 22 years old and currently studying at the
+        Coding Academy`s Bootcamp.
       </p>
+      <p>
+        Im looking forward to containuing to devleop my programming skills and
+        knowledge
+      </p>
+      <h1>Visit my social media</h1>
+      <a href="https://www.linkedin.com/in/nave-david-01527a2a6/">LinkedIn</a>
+      <a href="https://github.com/NaveDavid19">GitHub</a>
     </section>
   )
 }
